bfc6ee0a536c2ddb44d1027be705ab3fea727e5c
unknown <>
2016-03-01 23:30:08 +0800

    fix jsoncpp float value

--- jsoncpp/src/json_writer.cpp
+++ jsoncpp/src/json_writer.cpp
@@ -114,13 +114,13 @@ std::string valueToString(double value) {
                                                       // visual studio 2005 to
                                                       // avoid warning.
 #if defined(WINCE)
-  len = _snprintf(buffer, sizeof(buffer), "%.17g", value);
+  len = _snprintf(buffer, sizeof(buffer), "%.17lf", value);
 #else
-  len = sprintf_s(buffer, sizeof(buffer), "%.17g", value);
+  len = sprintf_s(buffer, sizeof(buffer), "%.17lf", value);
 #endif
 #else
   if (isfinite(value)) {
-    len = snprintf(buffer, sizeof(buffer), "%.17g", value);
+    len = snprintf(buffer, sizeof(buffer), "%.17lf", value);
   } else {
     // IEEE standard states that NaN values will not compare to themselves
     if (value != value) {
