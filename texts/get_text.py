0396f4aa3bd0255063037040dc3f85dfedf45553
number201724 <number201724@me.com>
2016-03-22 19:19:34 +0800

    text python

--- texts/get_text.py
+++ texts/get_text.py
@@ -1,12 +1,18 @@
 import json
 import codecs
+import sys
 
-f = file(r'01_com_027_01.ks.psb.json')
+if len(sys.argv) < 3:
+    print 'get_text.py <source_json> <target_text>'
+    exit(0)
+
+
+f = codecs.open(sys.argv[1], encoding='utf-8')
 
 script_data = json.load(f)
 objects_keys = script_data.keys()
 
-texts_file = codecs.open('01_com_027_01.txt', mode='w', encoding='utf-8')
+texts_file = codecs.open(sys.argv[2], mode='w', encoding='utf-8')
 
 if u'scenes' not in script_data:
     print 'invalid json script'
@@ -26,3 +32,5 @@ for i in scenes:
                     texts_file.writelines('//' + j[x].replace('\n', '\\n') + '\n')
                     texts_file.writelines(j[x].replace('\n', '\\n') + '\n')
                     texts_file.writelines('\n')
+    else:
+        print 'not texts entry'
--- texts/set_text.py
+++ texts/set_text.py
@@ -1,12 +1,18 @@
 import json
 import codecs
+import sys
 
-f = file(r'01_com_027_01.ks.psb.json')
+
+if len(sys.argv) < 3:
+    print 'set_text.py <source_json> <merge_texts>'
+    exit(0)
+    
+f = codecs.open(sys.argv[1], encoding='utf-8')
 
 script_data = json.load(f)
 objects_keys = script_data.keys()
 
-texts_file = codecs.open('01_com_027_01.txt', mode='r', encoding='utf-8')
+texts_file = codecs.open(sys.argv[2], mode='r', encoding='utf-8')
 
 if u'scenes' not in script_data:
     print 'invalid json script'
@@ -17,15 +23,21 @@ texts = []
 
 def parse_texts_file():
     while True:
-        tag = texts_file.readline()
+        magic = texts_file.readline()
+        
+        if not magic:
+            break
+
+        if magic[0] != '@':
+            continue
+
         check_text = texts_file.readline()
         cn_text = texts_file.readline()
         nul_line = texts_file.readline()
-
-        if not nul_line or not tag or not check_text or not cn_text:
+        if not check_text or not cn_text or not nul_line:
             break
-
-        str_id = int(tag.replace('\n', '').replace('@', ''))
+        
+        str_id = int(magic.replace('\n', '').replace('@', ''))
         check_text = check_text.replace('\n', '').replace('\\n', '\n')
         cn_text = cn_text.replace('\n', '').replace('\\n', '\n')
         str_rep = {'id': str_id, 'check_text': check_text, 'cn_text': cn_text}
@@ -49,14 +61,19 @@ def replace_text_to_json():
             for j in j_texts:
                 for x in range(0, 3):
                     if j[x] is not None and j[x] != '':
-                        j[x] = get_text(index)
+                        new_text = get_text(index)
+                        if not new_text:
+                            continue
+                        j[x] = new_text
                         index += 1
 
 
 parse_texts_file()
 replace_text_to_json()
 
-complete_file = codecs.open('01_com_027_01.new.json', mode='w', encoding='utf-8')
+f.close()
+
+complete_file = codecs.open(sys.argv[1], mode='w', encoding='utf-8')
 complete_file.writelines(json.dumps(script_data, ensure_ascii=False, indent=4, sort_keys=False))
 complete_file.flush()
 complete_file.close()
